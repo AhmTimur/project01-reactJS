78bdf31fd9a931e88ff259c4dcd3fc6b263b6151
Lordcakes <Ahmetzyanovtimur3@gmail.com>
2020-02-15 19:28:04 +0200

    layout

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img className='content-img' src='https://www.ferienwohnung-zehentner.at/site/assets/files/1022/steinberge.1200x200.jpg' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          Post 1
+        </div>
+        <div>
+          Post 2
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+}
+
+export default Profile;
