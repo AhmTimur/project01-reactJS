85caca28faf104f808a1b743e7d0e32b9e5dab1e
Lordcakes <Ahmetzyanovtimur3@gmail.com>
2020-02-21 22:15:04 +0200

    dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,15 +3,16 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts.jsx';
 
 const Profile = () => {
-  return <div className={s.content}>
-    <div>
-      <img className={s.content_img} src='https://www.ferienwohnung-zehentner.at/site/assets/files/1022/steinberge.1200x200.jpg' />
+    return <div>
+        <div>
+            <img className={s.content_img}
+                 src='https://www.ferienwohnung-zehentner.at/site/assets/files/1022/steinberge.1200x200.jpg'/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <MyPosts/>
     </div>
-    <div>
-      ava + description
-    </div>
-    <MyPosts />
-  </div>
 
 }
 
