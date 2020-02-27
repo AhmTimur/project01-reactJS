5d3bd7f7f22e6e77755ea35908ffcd2704b8a313
Lordcakes <Ahmetzyanovtimur3@gmail.com>
2020-02-27 21:24:46 +0200

    changed the desing

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,17 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>Timur</div>
+                <div className={s.dialog}>Sasha</div>
+                <div className={s.dialog}>Pasha</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hey</div>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>How are you doing?</div>
+            </div>
         </div>
     );
 }
