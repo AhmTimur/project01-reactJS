cc678238cc7f9f10cf5e2b4d90fc728d840adf8e
Lordcakes <Ahmetzyanovtimur3@gmail.com>
2020-03-28 19:08:59 +0200

    ref, onClick event

--- src/components/AppContent/Dialogs/Dialogs.jsx
+++ src/components/AppContent/Dialogs/Dialogs.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogsItem";
-import Message from "./Message/Message";
+import {NavLink} from "react-router-dom";
 
 
 const Dialogs = (props) => {
     let dialogElement = props.dialogs.map ( d => (<DialogItem name={d.name} id={d.id}/>));
 
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        let messageText = newMessageElement.current.value;
+        alert(messageText);
+    }
 
     return (
         <div className={s.dialogs}>
@@ -14,9 +20,10 @@ const Dialogs = (props) => {
                 <input type="text" placeholder='search'/>
             </div>
             <div className={s.dialogsItems}>
-                {dialogElement}
+                <NavLink to={'dialog' + dialogElement.id}>{dialogElement}</NavLink>
+                <div><textarea ref={newMessageElement}></textarea></div>
+                <button onClick={addMessage}>Send Message</button>
             </div>
-
         </div>
     );
 }
